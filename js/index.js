13e5b2dbf5c1b50c0e7105c2fda5dbcce2e95a82
OvBd1 <y.bontrond@ecole-ipssi.net>
2023-11-28 09:12:56 +0100

    ajout de js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const mainTitle = document.getElementById('main-title');
+
+mainTitle.addEventListener('click', () => {
+    if (mainTitle.style.color = 'red';) {
+        mainTitle.style.color = '#253a62';
+    } else if (mainTitle.style.color = '#253a62') {
+        mainTitle.style.color = 'red';
+    }
+
+})
